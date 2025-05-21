3c4630761a4ca5a9c8cc6458d22487ec8fabdb6d
alvar.dev <alvaropedrozo07@gmail.com>
2025-05-21 13:56:11 -0500

    fix: update mailer configuration to use Gmail service

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -4,13 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  }
+    pass: process.env.MAIL_PASS,
+  },
 });
 
 export const sendReservationEmail = async (toEmail, { name, date, time, people, table_id }) => {
