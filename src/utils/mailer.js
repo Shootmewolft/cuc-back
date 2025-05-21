5d0e6fe4ef5b0beabee5e4290ef018ce8c2ebb05
Keyner de Avila <kddeavila3@gmail.com>
2025-05-21 13:43:04 -0500

    fix: port and host of the mailer

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.ethereal.email",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
